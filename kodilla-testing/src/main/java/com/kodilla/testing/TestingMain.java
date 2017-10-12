8cfca16a900b79712602c1751f008f39f95ba429
Migakin <kinga.miga@gmail.com>
2017-10-12 13:07:34 +0200

    6.2 First unit test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,37 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+       SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+       String result = simpleUser.getUsername();
+
+       if (result.equals("theForumUser")){
+           System.out.println("test OK");
+       }
+       else{
+           System.out.println("Error!");
+       }
+
+       Calculator calculator = new Calculator();
+       int addResult = calculator.add(10, 20);
+       int substResult = calculator.subtract(20,10);
+
+       if (addResult == 30){
+           System.out.println("Addition test OK");
+       }
+       else{
+           System.out.println("Addition error");
+       }
+
+       if (substResult == 10){
+           System.out.println("Subtraction test OK");
+       }
+       else{
+           System.out.println("Subtraction error");
+       }
     }
 }
