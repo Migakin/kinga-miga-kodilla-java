e9625d2d231aaa87a92742701e6596f43c0cfb6d
“Migakin <kinga.miga@gmail.com>
2017-12-07 14:18:35 +0100

    *

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreMain.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreMain.java
@@ -1,6 +1,5 @@
 package com.kodilla.good.patterns.challenges;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.*;
@@ -8,9 +7,11 @@ import java.util.stream.*;
 public class MovieStoreMain {
     public static void main (String[] args) {
        MovieStore movieStore = new MovieStore();
-       movieStore.getMovies().entrySet().stream()
+       String movieList = movieStore.getMovies().entrySet().stream()
                 .map(Map.Entry::getValue)
                 .flatMap(List::stream)
-                .forEach(s -> System.out.print(s + "!"));
+                .collect(Collectors.joining("!"));
+        System.out.println(movieList);
+
     }
 }
